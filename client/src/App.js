fba2f9ed10972ae44871a3b5883da0f7afd09515
Md-Asikur <asikurrahaman997@gamil.com>
2022-12-10 19:53:08 -0800

    depploy

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import BottomNav from "./components/BottomNav";
+import Loading from "./components/Loading";
+import ClusterMap from "./components/map/ClusterMap";
+import NavBar from "./components/NavBar";
+import Notification from "./components/Notification";
+import Room from "./components/rooms/Room";
+import Login from "./components/user/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      {/* <ClusterMap/>
+      <Loading />
+      <Notification />
+      <BrowserRouter>
+        <Login />
+        <NavBar />
+        <BottomNav />
+      </BrowserRouter>
+      <Room /> */}
+      <Loading />
+      <Notification />
+      <BrowserRouter>
+        <Routes>
+          <Route path="dashboard/*" element={<Dashboard />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <Room />
+    </>
+  );
+}
+
+export default App;
---
+++ server/routes/roomRouter.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import { createRoom, deleteRoom, getRooms } from '../controllers/room.js';
+import auth from '../middleware/auth.js';
+
+const roomRouter = Router();
+roomRouter.post('/',auth, createRoom);//auth
+roomRouter.get('/', getRooms);
+roomRouter.delete('/:roomId', deleteRoom);
+export default roomRouter;
